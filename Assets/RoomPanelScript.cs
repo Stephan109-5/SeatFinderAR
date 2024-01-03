7d52b366848d2802ef737f039c3f333d6fb43911
Stephan109-5 <t109990005@ntut.org.tw>
2024-01-03 17:43:34 +0800

    RoomPanel Function Done

--- Assets/RoomPanelScript.cs
+++ Assets/RoomPanelScript.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Database;
+using Firebase.Extensions;
+using System;
+
 public class RoomPanelScript : MonoBehaviour
 {
     private string RoomName;
@@ -15,12 +20,47 @@ public class RoomPanelScript : MonoBehaviour
         RoomName = SceneManager.GetActiveScene().name;
         RoomNameLabel = GameObject.Find("RoomName").GetComponent<TextMeshProUGUI>();
         RoomNameLabel.text = RoomName;
-
+        /*GetRoomOccupancy();*/
+        
+        FirebaseDatabase.DefaultInstance.GetReference("rooms/" + RoomName + "/seats").ValueChanged += GetRoomOccupancy;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void GetRoomOccupancy(object sender, ValueChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+
+        DataSnapshot snapshot = args.Snapshot;
+        // Do something with snapshot...
+
+        Dictionary<string, object> data = (Dictionary<string, object>)snapshot.Value;
+        int occupants = 0;
+        int totalSeats = 0;
+        foreach (var seats in data)
+        {
+            /*Debug.Log($"{seats.Key}: {seats.Value}");
+            Debug.Log($"{seat_data["occupant"]}");*/
+            Dictionary<string, object> seat_data = (Dictionary<string, object>)seats.Value;
+                    
+            totalSeats++;
+
+            if (!string.IsNullOrEmpty((string) seat_data["occupant"]))
+            {
+                occupants++;
+            }
+
+        }
+
+        Debug.Log("Number of seat available: " +  occupants + "/" + totalSeats);
+
     }
 }
