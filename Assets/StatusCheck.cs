4f61d81f8b77ccab97c5aa5058683c22aaeeea39
Stephan109-5 <t109990005@ntut.org.tw>
2024-01-07 12:58:15 +0800

    Final Fix

--- Assets/StatusCheck.cs
+++ Assets/StatusCheck.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 
 public class StatusCheck : MonoBehaviour
 {
-    private TextMeshProUGUI statusText;
+    /*private TextMeshProUGUI statusText;*/
     private Firebase.FirebaseApp app;
     // Start is called before the first frame update
     void Awake()
     {
-        statusText = GameObject.Find("Status").GetComponent<TextMeshProUGUI>();
+        /*statusText = GameObject.Find("Status").GetComponent<TextMeshProUGUI>();*/
         Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
             var dependencyStatus = task.Result;
             if (dependencyStatus == Firebase.DependencyStatus.Available)
@@ -18,15 +18,16 @@ public class StatusCheck : MonoBehaviour
                 // Create and hold a reference to your FirebaseApp,
                 // where app is a Firebase.FirebaseApp property of your application class.
                 app = Firebase.FirebaseApp.DefaultInstance;
-                statusText.text = "Firebase: Online";
+
                 // Set a flag here to indicate whether Firebase is ready to use by your app.
+                /*statusText.text = "Firebase: Online";*/
             }
             else
             {
                 UnityEngine.Debug.LogError(System.String.Format(
                   "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
                 // Firebase Unity SDK is not safe to use here.
-                statusText.text = "Firebase: Error";
+                /*statusText.text = "Firebase: Error";*/
             }
         });
 
